5347dcd983e461dce9747338fbd6758d9b99c3e9
zeitmeister <simon.sporrong@hotmail.se>
2018-10-01 22:27:06 +0200

    ändringar enligt treehouse

--- app.js
+++ app.js
@@ -1,15 +1,46 @@
-const desc = "I just learned to create a node and to render it to the DOM";
-const myTitleID= 'main-title'
-const name = 'Simon';
-
-const header = (
+const Header = () => {
+  return (
     <header>
-        <h1 id={myTitleID}>{ name }'s First React Element</h1>
-        <p>{ desc }</p>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
     </header>
-);
+  );
+}
+
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        SimmeKingen
+      </span>
+
+      <Counter />
+    </div>
+  );
+}
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">35</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+}
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/*players list */}
+      <Player />
+    </div>
+  );
+}
 
 ReactDOM.render(
-    header,
+    <App />,
     document.getElementById('root')
 );
