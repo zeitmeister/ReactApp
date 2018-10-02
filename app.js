731550d6532f8e81bebc853eb1b20fa0270b9c52
Simon Sporrong <sps004@lul-net.ad.lul.se>
2018-10-02 13:56:40 +0200

    Senaste ändringar enligt treehousen

--- app.js
+++ app.js
@@ -1,46 +1,71 @@
-const Header = () => {
+const players = [
+    {
+      name: "Guil",
+      score: 50
+    },
+    {
+      name: "Treasure",
+      score: 85
+    },
+    {
+      name: "Ashley",
+      score: 95
+    },
+    {
+      name: "James",
+      score: 80
+    }
+]
+
+const Header = (props) => {
   return (
     <header>
-      <h1>Scoreboard</h1>
-      <span className="stats">Players: 1</span>
+      <h1>{ props.title }</h1>
+      <span className="stats">Players: {props.totalPlayers(5)}</span>
     </header>
   );
 }
 
-const Player = () => {
+const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
-        SimmeKingen
+        {props.playerName}
       </span>
 
-      <Counter />
+      <Counter score={props.playerScore}/>
     </div>
   );
 }
 
-const Counter = () => {
+const Counter = (props) => {
   return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">35</span>
+      <span className="counter-score">{props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
   );
 }
 
-const App = () => {
+const App = (props) => {
   return (
     <div className="scoreboard">
-      <Header />
+      <Header
+       title="Scoreboard"
+       totalPlayers={ props.initialPlayers.length } />
 
       {/*players list */}
-      <Player />
+      {props.initialPlayers.map( player => 
+        <Player 
+      playerName={player.playerName}
+      playerScore={player.playerScore}/>
+      )}
     </div>
   );
 }
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players}/>,
     document.getElementById('root')
 );
